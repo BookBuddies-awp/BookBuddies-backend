2fa0307a4e4f9c5c5be06c0a40a2718cb6b2f56f
dhruvkelawala <dhruvrk2000@gmail.com>
2020-12-31 13:13:58 +0530

    🎨 Setup the Folder Structure for MVC Pattern

--- app.js
+++ app.js
@@ -5,15 +5,14 @@
 // To start the app use `npm start` in the command prompt and go to `localhost:3000` on the browser
 
 const express = require('express'); // Import Express
+const router = require('./routes');
 
 const app = express(); // Create an Express App
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use('/', (req, res, next) => {
-  res.send('Hello World');
-});
+app.use(router);
 
 app.listen(3000, () => {
   console.log('App listening on port 3000!');
