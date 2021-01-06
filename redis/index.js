09e5c206cf6b75360d291fcd124bcd83191e95fa
dhruvkelawala <dhruvrk2000@gmail.com>
2021-01-06 15:21:35 +0530

    Add Redis Cache from AWS

--- redis/index.js
+++ redis/index.js
@@ -1,7 +1,10 @@
 const redis = require('redis');
 const { promisify } = require('util');
 
-const client = redis.createClient({ port: 6379 });
+const client = redis.createClient({
+  host: 'bookbuddies-redis.keiqgf.ng.0001.aps1.cache.amazonaws.com',
+  port: '6379',
+});
 
 exports.redisGet = promisify(client.get).bind(client);
 exports.redisSet = promisify(client.set).bind(client);
