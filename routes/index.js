f1eb421f0da8e59314edb46be1160a98ffba1be3
dhruvkelawala <dhruvrk2000@gmail.com>
2021-01-08 00:56:34 +0530

    🎨 Add Book Details Endpoint
    - Not useful for now

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,18 @@ const router = express.Router();
 const homeController = require('../controllers');
 const bestsellersController = require('../controllers/bestsellersController');
 const searchController = require('../controllers/searchController');
+const bookController = require('../controllers/bookController');
 
 /* GET homepage. */
 router.get('/', homeController);
 
-/* GET Recommended Books */
+/* GET Recommended Books Endpoint*/
 router.get('/api/bestsellers', bestsellersController);
 
 /* GET Search Endpoint */
-router.get('/api/search', searchController)
+router.get('/api/search', searchController);
+
+/* GET Book Details Endpoint */
+router.get('/api/book', bookController);
 
 module.exports = router;
