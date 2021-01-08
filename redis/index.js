f77e128ec3c8121f486dae6dc657a9b67ea41432
dhruvkelawala <dhruvrk2000@gmail.com>
2021-01-08 20:26:02 +0530

    🎨 Use REDIS env variable instead of actual string for security reasons.

--- redis/index.js
+++ redis/index.js
@@ -2,8 +2,7 @@ const redis = require('redis');
 const { promisify } = require('util');
 
 const client = redis.createClient({
-  url:
-    'redis://:p84552eaa0fd7ef9cf9232e0fe974ba32a406e569c251747bf8677c6875025ac9@ec2-50-17-242-160.compute-1.amazonaws.com:20589',
+  url: process.env.REDIS_URL,
 });
 
 exports.redisGet = promisify(client.get).bind(client);
